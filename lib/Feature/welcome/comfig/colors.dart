cccde942b7a68797ad9f291b8734a612deea7eb5
Inoyatovlee <150606428+Inoyatovlee@users.noreply.github.com>
2025-02-25 15:25:28 +0500

    helpers and colors- #ranglar ish..

---
+++ lib/Feature/welcome/comfig/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+import 'package:wat_sa/Feature/welcome/comfig/helpers.dart';
+
+class AppColor {
+  static Color green = HexColor.fromHex("#092635");
+  static Color white = HexColor.fromHex("#F6F5F5");
+  static Color green1 = HexColor.fromHex("#1B4242");
+  static Color green2 = HexColor.fromHex("#5C8374");
+  static Color green3 = HexColor.fromHex("#9EC8B9");
+  static Color di = HexColor.fromHex("#CDFFB6");
+  static Color green4 = HexColor.fromHex("#0BCE83");
+  static Color blue3 = Color.fromARGB(126, 194, 220, 252);
+  static Color blue1 = Color.fromARGB(180, 125, 130, 233);
+  static Color chec = HexColor.fromHex("#7203FF");
+}
